0f33579bd981bb99a6d8dfbe6160771fe518e464
EmyCodez <130977482+EmyCodez@users.noreply.github.com>
2025-08-18 19:38:23 +0400

    Update main.cpp

--- CPP09/ex02/main.cpp
+++ CPP09/ex02/main.cpp
@@ -1,17 +1,35 @@
 #include "PmergeMe.hpp"
+#include <ctime>
+#include <iostream>
 
 int main(int argc, char** argv)
 {
-	PmergeMe sorter;
+    clock_t totalStart = std::clock();  // START total timing
 
-	if (sorter.processInput(argc, argv))
-		return 1;
+    PmergeMe sorter;
+    if (sorter.processInput(argc, argv))
+        return 1;
 
-	std::vector<int> vec = sorter.getVector();
-	std::list<int> lst = sorter.getList();
+    std::vector<int> vec = sorter.getVector();
+    std::list<int> lst = sorter.getList();
 
-	sorter.performMergeSortVector(vec);
-	sorter.performMergeSortList(lst);
+    clock_t sortVecStart = std::clock();
+    sorter.performMergeSortVector(vec); // includes timing for sorting vector
+    clock_t sortVecEnd = std::clock();
 
-	return 0;
+    clock_t sortListStart = std::clock();
+    sorter.performMergeSortList(lst);   // includes timing for sorting list
+    clock_t sortListEnd = std::clock();
+
+    clock_t totalEnd = std::clock();    // END total timing
+
+    // Total durations (input + sort) in microseconds
+    double totalDuration = double(totalEnd - totalStart) * 1000000.0 / CLOCKS_PER_SEC;
+    double totalVecDuration = double(sortVecEnd - totalStart) * 1000000.0 / CLOCKS_PER_SEC;
+    double totalListDuration = double(totalEnd - sortVecEnd) * 1000000.0 / CLOCKS_PER_SEC;
+
+    std::cout << "Total time for std::vector operations: " << totalVecDuration << " us" << std::endl;
+    std::cout << "Total time for std::list operations: "   << totalListDuration << " us" << std::endl;
+
+    return 0;
 }
